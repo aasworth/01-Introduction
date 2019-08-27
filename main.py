980dcc4eaef03b2e1d42a4c42c483bc874c3b034
Aaron Worthington <aasworth@iu.edu>
2019-08-27 00:44:17 -0400

    edits licence, readme and finishes assignment

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Aaron Worthington.')
+print('\nMy favorite game is either Fallout 3, or really any simulation type games.')
+print('\nAny concerns I have about this class is possibly surpassing any creative mindblocks I may have')
+print('in trying to think of games to create or what to put inside of a game I will make.')
+print('\nI am honestly excited about learning more in the coding world and game design world')
+print('and I also am always excited to spend more time on my motorcycle and finding new roads and scenery.')
+print('\nMy stackoverflow.com user number is user:11980865.')
+print('\nThe URL to my github.com profile is https://github.com/aasworth.\n')
